3db47e709a5860061ed07b0ce1734ff1643105ea
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2018-01-16 21:51:50 +0200

    Prototyping

--- src/mockdata/mockpages.ts
+++ src/mockdata/mockpages.ts
@@ -2,13 +2,20 @@ var pages = {
 
     mainpage: {
         path: "index",
+        title: "Index page of oCMS",
         content: `
         <h1>Welcome to oCMS!</h1>        
         `
     },
     about:{
         path: "about",
+        title: "oCMS - About",
         content: "&copy; Nevahen"
+    },
+    errorpage:{
+        path: "404",
+        title: "404 - Page not found",
+        content: "<div class='col-md-12'><h1>404!</h1><p>Oops, seems like we don't have what you are looking for..</p></div>"
     }
 
 
