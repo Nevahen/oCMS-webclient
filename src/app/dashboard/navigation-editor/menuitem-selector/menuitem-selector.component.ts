a153e436bbfef60ccc645534dcc8f3a272e36703
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2018-01-31 13:03:46 +0200

    NavigationEditor stuff

--- src/app/dashboard/navigation-editor/menuitem-selector/menuitem-selector.component.ts
+++ src/app/dashboard/navigation-editor/menuitem-selector/menuitem-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { NavItem } from '../../../../models/navitem';
 
 @Component({
   selector: 'menuitem-selector',
@@ -8,8 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class MenuitemSelectorComponent implements OnInit {
 
   constructor() { }
+  name:string;
+  target:number;
+
+  @Input() index;
+  @Input() pageData;
+
+  @Output() onNavItemChange = new EventEmitter();
+
+  // Emit changes when value is being changed
+  onChange(value, type){  
+
+    if(type == "name"){
+      this.name = value;
+    }
+    
+    if(type == "target"){
+      this.target = value;
+    }
+
+    let navItem:NavItem =
+
+    {
+      name:this.name,
+      target:this.target,
+      children:[]
+    }
+
+    let data = {
+      index:this.index,
+      navItem:navItem
+    }
+
+    this.onNavItemChange.emit(data);
+  }
 
   ngOnInit() {
   }
-
 }
--- src/app/dashboard/navigation-editor/navigation-editor.component.ts
+++ src/app/dashboard/navigation-editor/navigation-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PageService } from '../../page.service';
+import { NavItem } from '../../../models/navitem';
 
 @Component({
   selector: 'navigation-editor',
@@ -17,6 +18,33 @@ export class NavigationEditorComponent implements OnInit {
   }
 
   pageData;
+
+  navData = {
+    mainpage:null,
+    errorpage:null,
+    nav_items:[]
+  };
+
+  onAddMenuItem(){
+
+    let navItem = new NavItem();
+    navItem.name = null;
+    navItem.target = null;
+
+    this.navData.nav_items.push(navItem);
+    console.log(this.navData);
+  }
+
+  onNavItemChange(value){
+    this.updateNavData(value.index, value.navItem);
+  }
+
+  updateNavData(index,data:NavItem){
+    // FIX ME: Resets menuitems when updating.. 
+      this.navData.nav_items[index].name =  data.name;
+      this.navData.nav_items[index].target =  data.target;
+      console.log(this.navData.nav_items);
+  }
 
 
   getPages(){
